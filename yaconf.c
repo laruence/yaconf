950ed708b9da956025597c006a202ce1a0dc8b3f
Xinchen Hui <laruence@gmail.com>
2016-12-01 18:13:33 +0800

    Fixed issue #20 (Segfault during test suite with 7.1.0GA (TS))

--- yaconf.c
+++ yaconf.c
@@ -527,7 +527,7 @@ PHP_MINIT_FUNCTION(yaconf)
 				            ZVAL_UNDEF(&active_ini_file_section);
 							YACONF_G(parse_err) = 0;
 							php_yaconf_hash_init(&result, 128);
-							if (zend_parse_ini_file(&fh, 0, 0 /* ZEND_INI_SCANNER_NORMAL */,
+							if (zend_parse_ini_file(&fh, 1, 0 /* ZEND_INI_SCANNER_NORMAL */,
 									php_yaconf_ini_parser_cb, (void *)&result) == FAILURE || YACONF_G(parse_err)) {
 								if (!YACONF_G(parse_err)) {
 									php_error(E_WARNING, "Parsing '%s' failed", ini_file);
@@ -622,12 +622,12 @@ PHP_RINIT_FUNCTION(yaconf)
 							ZVAL_UNDEF(&active_ini_file_section);
 							YACONF_G(parse_err) = 0;
 							php_yaconf_hash_init(&result, 128);
-							if (zend_parse_ini_file(&fh, 0, 0 /* ZEND_INI_SCANNER_NORMAL */,
+							if (zend_parse_ini_file(&fh, 1, 0 /* ZEND_INI_SCANNER_NORMAL */,
 									php_yaconf_ini_parser_cb, (void *)&result) == FAILURE || YACONF_G(parse_err)) {
-								YACONF_G(parse_err) = 0;
 								if (!YACONF_G(parse_err)) {
 									php_error(E_WARNING, "Parsing '%s' failed", ini_file);
 								}
+								YACONF_G(parse_err) = 0;
 								php_yaconf_hash_destroy(Z_ARRVAL(result));
 								free(namelist[i]);
 								continue;
