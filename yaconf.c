fc403739577714fd160d35713013e8bdcbd363e9
Xinchen Hui <laruence@gmail.com>
2020-02-18 18:48:01 +0800

    Restore unintensionally remove

--- yaconf.c
+++ yaconf.c
@@ -88,7 +88,7 @@ static void php_yaconf_hash_init(zval *zv, size_t size) /* {{{ */ {
 	HashTable *ht;
 	PALLOC_HASHTABLE(ht);
 	/* ZVAL_PTR_DTOR is necessary in case that this array be cloned */
-	zend_hash_init(ht, size, NULL, NULL, 1);
+	zend_hash_init(ht, size, NULL, ZVAL_PTR_DTOR, 1);
 #if PHP_VERSION_ID < 70300
 	GC_FLAGS(ht) |= (IS_ARRAY_IMMUTABLE | HASH_FLAG_STATIC_KEYS);
 #else
